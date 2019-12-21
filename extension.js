d533b564c6025edf82f03bef4453a2a0f4ecefde
Ansari-Tech <sansari1@radford.edu>
2019-12-21 18:50:28 -0500

    csharp support
    Added support for csharp getters and setters
    
    added switch to support more languages in the future

--- extension.js
+++ extension.js
@@ -1,120 +1,129 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
-function activate(context) 
-{
+function activate(context) {
+	let disposable = vscode.commands.registerCommand(
+		"extension.generateGetterAndSetters",
+		function() {
+			var editor = vscode.window.activeTextEditor;
+			if (!editor) return; // No open text editor
 
-    let disposable = vscode.commands.registerCommand('extension.generateGetterAndSetters', function () 
-    {
-        var editor = vscode.window.activeTextEditor;
-        if (!editor)
-            return; // No open text editor
+			var selection = editor.selection;
+			var text = editor.document.getText(selection);
 
-        var selection = editor.selection;
-        var text = editor.document.getText(selection);
+			if (text.length < 1) {
+				vscode.window.showErrorMessage("No selected properties.");
+				return;
+			}
 
-        if (text.length < 1)
-        {
-            vscode.window.showErrorMessage('No selected properties.');
-            return;
-        }
+			try {
+				var getterAndSetter = createGetterAndSetter(text);
 
-        try 
-        {
-            var getterAndSetter = createGetterAndSetter(text);
+				editor.edit(edit =>
+					editor.selections.forEach(selection => {
+						edit.insert(selection.end, getterAndSetter);
+					})
+				);
 
-            editor.edit(
-                edit => editor.selections.forEach(
-                  selection => 
-                  {
-                    edit.insert(selection.end, getterAndSetter);
-                  }
-                )
-              );
+				// format getterAndSetter
+				vscode.commands.executeCommand("editor.action.formatSelection");
+			} catch (error) {
+				console.log(error);
+				vscode.window.showErrorMessage(
+					'Something went wrong! Try that the properties are in this format: "private String name;"'
+				);
+			}
+		}
+	);
 
-            // format getterAndSetter
-            vscode.commands.executeCommand('editor.action.formatSelection');
-        } 
-        catch (error) 
-        {
-            console.log(error);
-            vscode.window.showErrorMessage('Something went wrong! Try that the properties are in this format: "private String name;"');
-        }
-    });
-
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
-function toPascalCase(str) 
-{
-    return str.replace(/\w+/g,w => w[0].toUpperCase() + w.slice(1));
+function toPascalCase(str) {
+	return str.replace(/\w+/g, w => w[0].toUpperCase() + w.slice(1));
 }
 
-function createGetterAndSetter(textPorperties)
-{
-    var properties = textPorperties.split(/\r?\n/).filter(x => x.length > 2).map(x => x.replace(';', ''));
+function generateCsharp(type, attribute, Attribute) {
+	return `
+        \tpublic ${type} ${Attribute} {
+        \tget { return ${attribute}; };
+        \tset { ${attribute} = value; }
+        \t} `;
+}
 
-    var generatedCode = `
+function generateDefault(type, attribute, Attribute) {
+	return `
+        \tpublic ${type} get${Attribute}() {
+        \t\treturn this.${attribute};
+        \t}
+
+    \tpublic void ${
+			type == "Boolean" || type == "bool" ? "is" : "set"
+		}${Attribute}(${type} ${attribute}) {
+    \t\tthis.${attribute} = ${attribute};
+    \t}`;
+}
+
+function createGetterAndSetter(textPorperties) {
+	var properties = textPorperties
+		.split(/\r?\n/)
+		.filter(x => x.length > 2)
+		.map(x => x.replace(";", ""));
+
+	var generatedCode = `
 `;
-    for (let p of properties) 
-    {
-        while (p.startsWith(" ")) p = p.substr(1);
-        while (p.startsWith("\t")) p = p.substr(1);
+	for (let p of properties) {
+		while (p.startsWith(" ")) p = p.substr(1);
+		while (p.startsWith("\t")) p = p.substr(1);
 
-        let words = p.split(" ").map(x => x.replace(/\r?\n/, ''));
-        let type, attribute, Attribute = "";
-        let create = false;
-        
-        // if words == ["private", "String", "name"];
-        if (words.length > 2)
-        {
-            type = words[1];
-            attribute = words[2];
-            Attribute = toPascalCase(words[2]);
+		let words = p.split(" ").map(x => x.replace(/\r?\n/, ""));
+		let type,
+			attribute,
+			Attribute = "";
+		let create = false;
 
-            create = true;
-        }
-        // if words == ["String", "name"];
-        else if (words.length == 2)
-        {
-            type = words[0];
-            attribute = words[1];
-            Attribute = toPascalCase(words[1]);
-            
-            create = true;            
-        }
-        // if words == ["name"];
-        else if (words.length)
-        {
-            type = "Object";
-            attribute = words[0];
-            Attribute = toPascalCase(words[0]);
-            
-            create = true;            
-        }
+		// if words == ["private", "String", "name"];
+		if (words.length > 2) {
+			type = words[1];
+			attribute = words[2];
+			Attribute = toPascalCase(words[2]);
 
-        if (create)
-        {
+			create = true;
+		}
+		// if words == ["String", "name"];
+		else if (words.length == 2) {
+			type = words[0];
+			attribute = words[1];
+			Attribute = toPascalCase(words[1]);
 
-            let code = 
-`
-\tpublic ${type} get${Attribute}() {
-\t\treturn this.${attribute};
-\t}
+			create = true;
+		}
+		// if words == ["name"];
+		else if (words.length) {
+			type = "Object";
+			attribute = words[0];
+			Attribute = toPascalCase(words[0]);
 
-\tpublic void ${type == "Boolean" || type == "bool" ? "is" : "set"}${Attribute}(${type} ${attribute}) {
-\t\tthis.${attribute} = ${attribute};
-\t}
-`;
-            generatedCode += code;
-        }
-    }
+			create = true;
+		}
 
-    return generatedCode;
+		if (create) {
+			switch (vscode.window.activeTextEditor.document.languageId) {
+				case "csharp":
+					generatedCode += generateCsharp(type, attribute, Attribute);
+					break;
+				default:
+					generatedCode += generateDefault(type, attribute, Attribute);
+					break;
+			}
+		}
+	}
+
+	return generatedCode;
 }
 
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 
 exports.deactivate = deactivate;
